ceba7a6e84d7240aead2b57b4b82e21b3c14edc6
Alessandro Mancinelli <alexm95@hotmail.it>
2020-09-07 09:46:54 +0200

    Creating Initial Coursera Repository

---
+++ README.md
@@ -0,0 +1,14 @@
+###******************************************************************************
+# Copyright (C) 2020 by Alessandro Mancinelli
+#
+# Redistribution, modification or use of this software in source or binary
+# forms is permitted as long as the files maintain this copyright. Users are 
+# permitted to modify this and use it to learn about the field of embedded
+# software. Alessandro Mancinelli is not liable for any misuse of this material. 
+#
+###*****************************************************************************
+
+
+### This is a free access repository of the student Alessandro 
+### Mancinelli's delivery folder for the Coursera Specialization 
+### of Embedded Software Essentials.
---
+++ stats.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Copyright (C) 2020 by Alessandro Mancinelli
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alessandro Mancinelli is not liable for any misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h
+ * @brief Computation of basic stat analisys to a set of data
+ *
+ * This code performs and print the minimum, maximum, mean and median of the 
+ * character array "test" defined in main. Moreover, it also generate a variable
+ * called "test_sorted" containing the sorted values of the variable "test"
+ *
+ * @author Alessandro Mancinelli
+ * @date 09/07/2020
+ */
+ 
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
